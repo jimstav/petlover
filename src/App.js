ebdd06eff2f49e30f122d3636ab364890eb68c2e
Dimitris Stavroulakis <jimstav@gmail.com>
2024-10-13 12:41:35 +0300

    Add HomePage as a nested route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,11 @@ import {
 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Root />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/" element={<HomePage />} />
+    </Route>
+  )
 );
 
 function App() {
